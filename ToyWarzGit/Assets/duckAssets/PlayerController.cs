e5337cbdfde392b5e774f0dac9f236c88013c128
wejdan <wejdan.alahmadi94@gmail.com>
2018-07-29 21:29:32 +0300

    adding duck + evil teddy scene

---
+++ ToyWarzGit/Assets/duckAssets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardeForce = 10f;
+    public Animator myAnimator;
+    public GameObject beak;
+   // public ParticleSystem particalLuncher;
+    public GameObject particalLaunch;
+
+    public bool moduleEnabled;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        //particalLuncher = GetComponent<ParticleSystem>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardeForce * Time.deltaTime);
+     /** if (beak.transform.position.y > 0.005671469)
+        {
+            particalLaunch.SetActive(true);
+        }
+        if (beak.transform.position.y <= 0.005671469)
+        {
+            particalLaunch.SetActive(false);
+
+        }  **/
+
+
+
+
+    } 
+
+    void waveAttackStart() {
+        particalLaunch.SetActive(true);
+       // particalLaunch.SetActive(false);
+    }
+
+    void waveAttackStop()
+    {
+       // particalLaunch.SetActive(true);
+        particalLaunch.SetActive(false);
+    }
+
+}
---
+++ ToyWarzGit/Assets/evilteddyAssets/script/cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+
+        if (target.tag == "shoot")
+        {
+
+            Time.timeScale = 0f;
+
+        }
+
+    }
+}
