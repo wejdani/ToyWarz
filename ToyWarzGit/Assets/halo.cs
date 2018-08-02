7466c49d9738c967d756f22a9dd69d6e92233444
wejdan <wejdan.alahmadi94@gmail.com>
2018-08-02 14:38:15 +0300

    Halo and technie asset update

---
+++ ToyWarzGit/Assets/halo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class halo : MonoBehaviour
+{
+
+    float i;
+    public Light light; 
+    // public float multiplier; 
+
+
+    // Use this for initialization
+    void Start()
+    {
+        light = GetComponent<Light>();
+        //StartCoroutine("WaitThreeSeconds");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dim();
+
+    }
+
+    void dim(){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //light.range -= 1;
+            for (i = 10; i <= 1; i--)
+            {
+
+                light.range = i;
+                //RenderSettings.haloStrength -= (float)0.1;
+                StartCoroutine("WaitThreeSeconds");
+                Debug.Log(i + "light range" + light.range);
+            }
+            // RenderSettings.haloStrength -= 1;
+        }
+    }
+    IEnumerator WaitThreeSeconds()
+    {
+
+   
+
+        Debug.Log("wait..");
+        yield return new WaitForSeconds(3);
+    }
+}
