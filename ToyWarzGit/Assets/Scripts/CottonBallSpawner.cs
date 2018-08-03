96734f97edcd4c23aeb8b092a7dba4fdb636eef8
wejdan <wejdan.alahmadi94@gmail.com>
2018-08-03 14:49:52 +0300

    remove unnecessary files

---
+++ ToyWarzGit/Assets/Scripts/CottonBallSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CottonBallSpawner : MonoBehaviour {
+
+    public GameObject CottonBallprojectile;
+    public float projectileSpeed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            GameObject cottonBall = Instantiate(CottonBallprojectile, transform) as GameObject;
+            Rigidbody rb = cottonBall.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * projectileSpeed;
+        }
+    }
+}
