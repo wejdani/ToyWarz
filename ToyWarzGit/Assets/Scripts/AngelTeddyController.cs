a5942ffbb953b687e13db6fed7ed76e499af1dd8
wejdan <wejdan.alahmadi94@gmail.com>
2018-08-04 13:58:08 +0300

    player controller

--- ToyWarzGit/Assets/Scripts/AngelTeddyController.cs
+++ ToyWarzGit/Assets/Scripts/AngelTeddyController.cs
@@ -7,13 +7,51 @@ public class AngelTeddyController : MonoBehaviour {
     public GameObject CottonBallprojectile;
     public float projectileSpeed;
 
+    public Animator animator;
+    private float inputH;
+    private float inputV;
+    public Rigidbody rbody;
+    private bool run;
+
+     void Start()
+    {
+        animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        run = false; 
+    }
     void Update()
     {
         //if (Input.GetKeyDown(KeyCode.Space))
         // {
-        shoot();
+        //shoot();
 
         //}
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+        animator.SetFloat("inputH", inputH);
+        animator.SetFloat("inputV", inputV);
+        animator.SetBool("run", run);
+        float moveX = inputH * 20f * Time.deltaTime; 
+        float moveZ = inputV * 50f * Time.deltaTime; 
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        if(moveZ<=0){
+            moveX = 0;
+        }
+        if(run){
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
+        rbody.velocity = new Vector3(moveX, 0, moveZ);
+
     }
 
     void shoot() {
